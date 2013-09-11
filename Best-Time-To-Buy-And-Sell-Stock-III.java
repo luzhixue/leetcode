483cf9bb4a311d2f81f660e16b8e058e9719222f
luzhixue <zhixuelu@gmail.com>
2013-09-11 19:36:28 -0400

    Create Best-Time-To-Buy-And-Sell-Stock-III.java

---
+++ Best-Time-To-Buy-And-Sell-Stock-III.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        if(prices == null || prices.length <= 1) return 0;
+        int[] maxOneProfit = new int[prices.length];
+        maxOneProfit[prices.length-1] = 0;
+        int maxValue = prices[prices.length-1];
+        for(int i = prices.length-2; i >= 0; i--){
+            maxValue = Math.max(maxValue,prices[i]);//maxValue from i to end
+            maxOneProfit[i] = Math.max(maxValue-prices[i],maxOneProfit[i+1]);//max possible profit from one transaction
+        }
+        int minValue = prices[0], firstProfit = 0, maxTotal = 0;
+        for(int i = 0; i < prices.length-1; i++){
+            minValue = Math.min(minValue, prices[i]);
+            firstProfit = Math.max(firstProfit, prices[i]-minValue);
+            maxTotal = Math.max(firstProfit+maxOneProfit[i],maxTotal);
+        }
+        return maxTotal;
+    }
+}
