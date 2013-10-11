3c0ab599a744843f857bde024b78f1a47296dece
luzhixue <zhixuelu@gmail.com>
2013-10-11 17:07:18 -0400

    Create Single-Number.java

---
+++ Single-Number.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int singleNumber(int[] A) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int number = 0;
+        for(int i = 0; i < A.length; i++){
+            number ^= A[i];
+        }
+        return number;
+    }
+}
