0e3cfb5f508c11921e15761016d0f9a7cf8da86f
luzhixue <zhixuelu@gmail.com>
2013-10-11 14:54:34 -0400

    Update Maximum-Depth-of-Binary-Tree.java

--- Maximum-Depth-of-Binary-Tree.java
+++ Maximum-Depth-of-Binary-Tree.java
@@ -7,10 +7,38 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+ 
+//recursive solution
 public class Solution {
     public int maxDepth(TreeNode root) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if(root == null) return 0;
         return Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
+    }
+}
+
+//BFS using a queue
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        if(root == null) return 0;
+        int depth = 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int curNum = 0, levelNum = 1;
+        while(queue.peek() != null){
+            TreeNode node = queue.poll();
+            ++curNum;
+            if(node.left != null)
+                queue.offer(node.left);
+            if(node.right != null)
+                queue.offer(node.right);
+            if(curNum == levelNum){
+                ++depth;
+                curNum = 0;
+                levelNum = queue.size();
+            }
+        }
+        return depth;
     }
 }
