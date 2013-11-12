3ed4f56c7d31e4013ae1a024ec05fe3cbc41fa2e
luzhixue <zhixuelu@gmail.com>
2013-11-12 18:53:59 -0500

    Update Partition-List.java

--- Partition-List.java
+++ Partition-List.java
@@ -1,3 +1,12 @@
+//Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+//You should preserve the original relative order of the nodes in each of the two partitions. 
+//For example,
+//Given 1->4->3->2->5->2 and x = 3,
+//return 1->2->2->4->3->5. 
+
+//Solution: create a small list and a large list, use a cur pointer to go through the original list, and add the node to
+//either small or large list. Finally merge those two lists.
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
