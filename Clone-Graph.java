041398136c120b2419754ee49c47fc082674d8c1
luzhixue <zhixuelu@gmail.com>
2014-01-12 01:30:49 -0500

    Update Clone-Graph.java

--- Clone-Graph.java
+++ Clone-Graph.java
@@ -6,6 +6,37 @@
  *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
  * };
  */
+ 
+ //to prevent infinite loop due to cycles (especially in directed graph), add the node to queue only if it is first time discovery
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if(node == null) return null;
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        map.put(node, new UndirectedGraphNode(node.label));
+        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode>();
+        queue.offer(node);
+        while(queue.size() > 0){
+            UndirectedGraphNode oldNode = queue.poll();
+            UndirectedGraphNode newNode = map.get(oldNode);
+            for(UndirectedGraphNode oldNeibor: oldNode.neighbors){
+                if(!map.containsKey(oldNeibor)){
+                    UndirectedGraphNode newNeibor = new UndirectedGraphNode(oldNeibor.label);
+                    map.put(oldNeibor, newNeibor);
+                    newNode.neighbors.add(newNeibor);
+                    queue.offer(oldNeibor);//discovered new Node
+                }
+                else{
+                    newNode.neighbors.add(map.get(oldNeibor));
+                }
+            }
+            
+        }
+        return map.get(node);        
+    }
+}
+ 
+ 
+//original working version
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
         if(node == null) return null;
@@ -33,7 +64,7 @@ public class Solution {
                         newNeibor = map.get(oldNeibor);
                     }
                     newNode.neighbors.add(newNeibor);
-                    queue.offer(oldNeibor);
+                    queue.offer(oldNeibor); //this is bad, because if you always add the node to queue, there could be cycles, thus result in infinite loops 
                 }
             }
         }
